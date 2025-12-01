f992d9a0bbf9a5e8cc49ad922340553ff11ac10d
_FlameVortex <160109584+FlameVortex@users.noreply.github.com>
2025-12-01 16:38:06 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,2 @@
-import base64 as __A, zlib as __B, marshal as __C, importlib, sys
-
-try:
-    __P = b"eJxLzs8tKEotLkktLlHISSxJT0rMz1FIT8xL1UjOzytJLEnMAwCDAwgO"
-    __D = __C.loads(__B.decompress(__A.b64decode(__P)))
-    exec(__D, globals(), globals())
-except Exception as E:
-    print("Error:", E)
-    sys.exit(1)
-
+import x9_core as flame
+flame.main()
